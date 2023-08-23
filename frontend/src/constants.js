6b432d8d52317adabff381f966e3fcef9d086333
Ken Sagala <skendrick@teratomo.com>
2023-08-23 23:46:39 +0800

    Updated fetching API / state management from Axios to Redux

---
+++ frontend/src/constants.js
@@ -0,0 +1,12 @@
+/* 
+    This file contains all the constants used in the frontend.
+    It is used to avoid hardcoding the same values in multiple places.
+*/
+
+// export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : ''
+
+export const BASE_URL = ''
+export const PRODUCTS_URL = '/api/products'
+export const USERS_URL = '/api/users'
+export const ORDERS_URL = '/api/orders'
+export const PAYPAL_URL = '/api/config/paypal'
