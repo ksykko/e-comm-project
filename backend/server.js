7f2c46c779af743e8c1ae5254be51aa42fc2e399
Ken Sagala <skendrick@teratomo.com>
2023-09-19 22:26:05 +0800

    Added cookie parser middleware

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@
 
 import express from 'express'
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
 dotenv.config()
 
 import connectDB from './config/db.js'
@@ -23,6 +24,9 @@ const app = express()
 // Body parser middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+// Cookie parser middleware
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
     res.send('API is running...')
