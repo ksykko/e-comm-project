6ea61b0aa0da972eb82c6662f3792be6da21f4ef
Ken Sagala <skendrick@teratomo.com>
2023-09-19 22:49:28 +0800

    Added a middleware that hash the password of newly registered user

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -31,9 +31,21 @@ const userSchema = new mongoose.Schema(
     }
 )
 
+//  This is a method that is used to compare the entered password with the hashed password in the database.
 userSchema.methods.matchPassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password)
 }
+
+// This is a middleware that runs before the save() method. It is used to hash the password before saving it to the database.
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next()
+    }
+
+    // Generate salt and hash password
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+})
 
 const User = mongoose.model('User', userSchema)
 
