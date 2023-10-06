74b5f533ea50eddba8c8c7eee12869c97d90f6e7
Ken Sagala <sagala.krch@gmail.com>
2023-10-06 23:11:41 +0800

    Added create product API and route

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -8,7 +8,7 @@ import Product from '../models/product.model.js'
 // @desc Fetch all products
 // @route GET /api/products
 // @access Public
-const getProducts = asyncHandler(async (req, res) => {
+const getProducts = asyncHandler(async(req, res) => {
     const products = await Product.find({}) // Get all products
 
     res.json(products)
@@ -17,7 +17,7 @@ const getProducts = asyncHandler(async (req, res) => {
 // @desc Fetch single product
 // @route GET /api/products/:id
 // @access Public
-const getProductById = asyncHandler(async (req, res) => {
+const getProductById = asyncHandler(async(req, res) => {
     const product = await Product.findById(req.params.id) // Get product by ID
 
     if (product) {
@@ -28,4 +28,24 @@ const getProductById = asyncHandler(async (req, res) => {
     }
 })
 
-export { getProducts, getProductById }
+// @desc Create a product
+// @route POST /api/products
+// @access Private/Admin
+const createProduct = asyncHandler(async(req, res) => {
+    const product = new Product({
+        name: 'Sample product',
+        price: 0,
+        user: req.user._id,
+        image: '/images/sample.jpg',
+        brand: 'Sample brand',
+        category: 'Sample category',
+        countInStock: 0,
+        numReviews: 0,
+        description: 'Sample description',
+    })
+
+    const createdProduct = await product.save()
+    res.status(201).json(createdProduct)
+})
+
+export { getProducts, getProductById, createProduct }
--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -1,14 +1,15 @@
 /* 
     This file is used to create the routes for the products.
-    The routes are then imported into backend\server.js
+    The routes are then imported into backend/server.js
 */
 
 import express from 'express'
-import { getProducts, getProductById } from '../controllers/product.controller.js'
+import { getProducts, getProductById, createProduct } from '../controllers/product.controller.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
-router.route('/').get(getProducts)
+router.route('/').get(getProducts).post(protect, admin, createProduct)
 router.route('/:id').get(getProductById)
 
 export default router
