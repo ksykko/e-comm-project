b4e936c154a0b6f8061d12b12d8719ab68f51ecd
Ken Sagala <sagala.krch@gmail.com>
2023-10-06 22:57:25 +0800

    Added update order to delivered API

--- backend/controllers/order.controller.js
+++ backend/controllers/order.controller.js
@@ -8,7 +8,7 @@ import Order from '../models/order.model.js'
 // @desc Create new order
 // @route POST /api/orders
 // @access Private
-const addOrderItems = asyncHandler(async (req, res) => {
+const addOrderItems = asyncHandler(async(req, res) => {
     const {
         orderItems,
         shippingAddress,
@@ -48,7 +48,7 @@ const addOrderItems = asyncHandler(async (req, res) => {
 // @desc Get logged in user orders
 // @route GET /api/orders/myorders
 // @access Private
-const getMyOrders = asyncHandler(async (req, res) => {
+const getMyOrders = asyncHandler(async(req, res) => {
     const orders = await Order.find({ user: req.user._id })
     res.status(200).json(orders)
 })
@@ -56,7 +56,7 @@ const getMyOrders = asyncHandler(async (req, res) => {
 // @desc Get order by ID
 // @route GET /api/orders/:id
 // @access Private
-const getOrderById = asyncHandler(async (req, res) => {
+const getOrderById = asyncHandler(async(req, res) => {
     //* The .populate() method is used to populate the user field with the name and email of the user.
     const order = await Order.findById(req.params.id).populate('user', 'name email')
 
@@ -71,7 +71,7 @@ const getOrderById = asyncHandler(async (req, res) => {
 // @desc Update order to paid
 // @route PUT /api/orders/:id/pay
 // @access Private
-const updateOrderToPaid = asyncHandler(async (req, res) => {
+const updateOrderToPaid = asyncHandler(async(req, res) => {
     const order = await Order.findById(req.params.id)
 
     if (order) {
@@ -96,14 +96,25 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
 // @desc Update order to delivered
 // @route PUT /api/orders/:id/deliver
 // @access Private/Admin
-const updateOrderToDelivered = asyncHandler(async (req, res) => {
-    res.send('Update order to delivered')
+const updateOrderToDelivered = asyncHandler(async(req, res) => {
+    const order = await Order.findById(req.params.id)
+
+    if (order) {
+        order.isDelivered = true
+        order.deliveredAt = Date.now()
+
+        const updatedOrder = await order.save()
+        res.status(200).json(updatedOrder)
+    } else {
+        res.status(404)
+        throw new Error('Order not found')
+    }
 })
 
 // @desc Get all orders
 // @route GET /api/orders
 // @access Private/Admin
-const getOrders = asyncHandler(async (req, res) => {
+const getOrders = asyncHandler(async(req, res) => {
     const orders = await Order.find({}).populate('user', 'id name')
     res.status(200).json(orders)
 })
