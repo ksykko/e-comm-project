487a2760e54c58ab7bb4a822099c5e74544bff17
Ken Sagala <sagala.krch@gmail.com>
2023-10-07 12:21:05 +0800

    Added upload image route

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@
     
 */
 
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
@@ -15,6 +16,7 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import productRoutes from './routes/product.routes.js'
 import userRoutes from './routes/user.routes.js'
 import orderRoutes from './routes/order.routes.js'
+import uploadRoutes from './routes/upload.routes.js'
 
 const port = process.env.PORT || 5000
 
@@ -36,8 +38,12 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
 app.get('/api/config/paypal', (req, res) => res.send({ clientId: process.env.PAYPAL_CLIENT_ID }))
+
+const __dirname = path.resolve() // Get the current directory name
+app.use('/uploads', express.static(path.join(__dirname, '/uploads'))) // Make the uploads folder static
 
 // This is used to handle errors
 app.use(notFound)
