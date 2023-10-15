2f9a4679986b5a5d55a9707755d707357e1237d0
Ken Sagala <sagala.krch@gmail.com>
2023-10-16 00:04:51 +0800

    Edit search home screen

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -27,7 +27,11 @@ const HomeScreen = () => {
                             </Col>
                         ))}
                     </Row>
-                    <Paginate pages={data.pages} page={data.page} />
+                    <Paginate
+                        pages={data.pages}
+                        page={data.page}
+                        keyword={keyword ? keyword : ''}
+                    />
                 </>
             )}
         </>
