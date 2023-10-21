89ca865d6403d33ef19d1a4f65bbdc6b28f62329
Ken Sagala <sagala.krch@gmail.com>
2023-10-21 23:55:13 +0800

    Added react helmet async

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -8,6 +8,7 @@ import {
 } from 'react-router-dom'
 
 import { PayPalScriptProvider } from '@paypal/react-paypal-js'
+import { HelmetProvider } from 'react-helmet-async'
 import { Provider } from 'react-redux'
 import store from './store'
 
@@ -68,11 +69,13 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
     <React.StrictMode>
-        <Provider store={store}>
-            <PayPalScriptProvider deferLoading={true}>
-                <RouterProvider router={router} />
-            </PayPalScriptProvider>
-        </Provider>
+        <HelmetProvider>
+            <Provider store={store}>
+                <PayPalScriptProvider deferLoading={true}>
+                    <RouterProvider router={router} />
+                </PayPalScriptProvider>
+            </Provider>
+        </HelmetProvider>
     </React.StrictMode>
 )
 
